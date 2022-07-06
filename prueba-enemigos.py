b56a40e7a6de685b2eaf4f1119473535292d647b
SnowieMischief <97817883+SnowieMischief@users.noreply.github.com>
2022-07-05 21:46:28 -0300

    commit

--- prueba-enemigos.py
+++ prueba-enemigos.py
@@ -45,7 +45,45 @@ prueba3 = probar(6,enemigos_mal,disparos_ok)[0]
 if ('PERDISTE' and 'errores') in prueba3:
     print('----------- Prueba 3 pass')
 
-if 'errores' in probar(6,enemigos_mal,disparos_mal)[0]:
+prueba4 = probar(6,enemigos_mal,disparos_mal)[0]
+if 'errores' in prueba4:
     print('----------- Prueba 4 pass')
 
 print()
+x = generar(11)
+enemigos_ok1 = [
+    'C1',
+    'C3',
+    'A2',
+    'B4',
+    'D2',
+]
+disparos_ok1 = [
+    'C1',
+    'C3',
+    'A2',
+    'B4',
+    'D2',
+]
+enemigos_mal1 = [
+    'A1',
+    'A2',
+    'G3',
+    'D4',
+    'C9',
+]
+disparos_mal1 = [ 
+    'A1',
+    'A2',
+    'G3',
+    'D4',
+    'C9',
+]
+
+prueba5 = probar(11,enemigos_ok1,disparos_ok1)[0]
+if "errores" in prueba5:
+      print('----------- Prueba 5 pass')
+
+prueba6 = probar(2,enemigos_ok1,disparos_ok1)[0]
+if "errores" in prueba6:
+      print('----------- Prueba 6 pass')
